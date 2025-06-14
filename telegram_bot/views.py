87986626f057e5414eecf7fe0487ca1c135360ae
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-06-15 02:49:54 +0300

    Create views.py

---
+++ telegram_bot/views.py
@@ -0,0 +1,27 @@
+import logging
+import os
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from telegram import Update
+from telegram.ext import Application
+
+from telegram_bot.handlers import setup_handlers
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+application = Application.builder().token(BOT_TOKEN).build()
+setup_handlers(application)
+
+logger = logging.getLogger(__name__)
+
+@csrf_exempt
+async def telegram_webhook(request):
+    if request.method == "POST":
+        try:
+            data = request.body.decode("utf-8")
+            update = Update.de_json(eval(data), application.bot)
+            await application.process_update(update)
+            return JsonResponse({"ok": True})
+        except Exception as e:
+            logger.exception("Ошибка в webhook:")
+            return JsonResponse({"ok": False, "error": str(e)}, status=400)
+    return JsonResponse({"ok": True})
