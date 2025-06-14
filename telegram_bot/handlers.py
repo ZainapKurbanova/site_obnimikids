90931bde00a71edba2542133fdb6af1808c52736
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-06-15 02:38:43 +0300

    Create handlers.py

---
+++ telegram_bot/handlers.py
@@ -0,0 +1,13 @@
+from telegram.ext import CommandHandler, MessageHandler, CallbackQueryHandler, filters
+from telegram_bot.views_logic import (
+    start_handler,
+    callback_query_handler,
+    text_handler,
+    media_handler,
+)
+
+def setup_handlers(application):
+    application.add_handler(CommandHandler("start", start_handler))
+    application.add_handler(CallbackQueryHandler(callback_query_handler))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
+    application.add_handler(MessageHandler(filters.PHOTO | filters.Document.ALL, media_handler))
