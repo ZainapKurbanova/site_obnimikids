2f5aaf50db915151484d77ecc01ccc6f0adbdab1
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-06-15 02:59:35 +0300

    Update views.py

--- telegram_bot/views.py
+++ telegram_bot/views.py
@@ -1,24 +1,27 @@
 import logging
 import os
+import json
+
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+
 from telegram import Update
-from telegram.ext import Application
+from telegram.ext import Application, ContextTypes
 
 from telegram_bot.handlers import setup_handlers
+
+logger = logging.getLogger(__name__)
 
 BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 application = Application.builder().token(BOT_TOKEN).build()
 setup_handlers(application)
 
-logger = logging.getLogger(__name__)
-
 @csrf_exempt
 async def telegram_webhook(request):
     if request.method == "POST":
         try:
-            data = request.body.decode("utf-8")
-            update = Update.de_json(eval(data), application.bot)
+            data = json.loads(request.body.decode("utf-8"))
+            update = Update.de_json(data, application.bot)
             await application.process_update(update)
             return JsonResponse({"ok": True})
         except Exception as e:
