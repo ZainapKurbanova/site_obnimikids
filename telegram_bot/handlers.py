4224a58de4a2576ce2b480df20cf166d664da362
ZainapKurbanova <160218230+ZainapKurbanova@users.noreply.github.com>
2025-06-15 03:53:52 +0300

    Update handlers.py

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -1,15 +1,18 @@
-from telegram.ext import Application
+from telegram.ext import (
+    CommandHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    filters,
+)
 from telegram_bot.views_logic import (
     start_handler,
     callback_query_handler,
     text_handler,
-    media_handler
+    media_handler,
 )
 
-def setup_handlers(app: Application):
-    from telegram.ext import CommandHandler, CallbackQueryHandler, MessageHandler, filters
-
-    app.add_handler(CommandHandler("start", start_handler))
-    app.add_handler(CallbackQueryHandler(callback_query_handler))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
-    app.add_handler(MessageHandler(filters.PHOTO | filters.Document.ALL, media_handler))
+def setup_handlers(application):
+    application.add_handler(CommandHandler("start", start_handler))
+    application.add_handler(CallbackQueryHandler(callback_query_handler))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, text_handler))
+    application.add_handler(MessageHandler(filters.PHOTO | filters.Document.IMAGE, media_handler))
